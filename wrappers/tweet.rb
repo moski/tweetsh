ef0cbe389e4d4f4a1ff1c4553d307211e1e93c6e
moski <abushaikh@gmail.com>
2010-02-09 20:26:30 +0200

    for some reason the order of requiring files between the server and my local machine is different, so requiring wrappers/user.b will fail if wrappers.base.rb is not loaded, so i added a require in the class

--- wrappers/tweet.rb
+++ wrappers/tweet.rb
@@ -1,3 +1,5 @@
+require 'wrappers/base.rb'
+
 # A wrapper class for the tweet json string response from twitter.
 # It inherts all the meta features from the parent class.
 class Tweet < Base
--- wrappers/user.rb
+++ wrappers/user.rb
@@ -1,3 +1,5 @@
+require 'wrappers/base.rb'
+
 # A wrapper class for the user json string response from twitter.
 # It inherts all the meta features from the parent class.
 class User < Base
